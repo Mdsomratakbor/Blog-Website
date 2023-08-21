c45bda333b453f29629f4227a0ae2adf69587375
MdSomratAkbor <somrat876@gmail.com>
2023-08-21 19:11:53 +0600

    project initialize

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='bg-slate-600 p-4 sticky top-0 
+    drop-shadow-xl z-10'>
+        <div className='prose prose-xl mx-auto flex justify-between
+        flex-col flex-row-sm'>
+           <Link href='/' className='text-white/90 no-underline hover:text-white'> Md Samrat Akbor </Link>
+
+        </div>
+    </nav>
+  )
+}
