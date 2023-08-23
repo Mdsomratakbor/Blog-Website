129eef84b09126d3939939bd083645238b581c07
MdSomratAkbor <somrat876@gmail.com>
2023-08-23 18:20:06 +0600

    route problem solve

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,5 +1,5 @@
-import getFormattedDate from "../lib/getFormattedDate"
-import { getSortedPostsData, getPostData } from "../lib/Post"
+import getFormattedDate from "../../lib/getFormattedDate"
+import { getSortedPostsData, getPostData } from "../../lib/Post"
 import { notFound } from "next/navigation"
 import Link from "next/link"
 
@@ -41,17 +41,17 @@ export default async function Post({ params }: { params: { postId: string } }) {
     const pubDate = getFormattedDate(date)
 
     return (
-        <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
-            <p className="mt-0">
-                {pubDate}
+        <main className="px-6 prose prose-xl prose-slate prose-invert mx-auto text-white">
+        <h1 className="text-3xl mt-4 mb-0 ">{title}</h1>
+        <p className="mt-0">
+            {pubDate}
+        </p>
+        <article>
+            <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+            <p>
+                <Link href="/">← Back to home</Link>
             </p>
-            <article>
-                <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-                <p>
-                    <Link href="/">← Back to home</Link>
-                </p>
-            </article>
-        </main>
+        </article>
+    </main>
     )
 }
